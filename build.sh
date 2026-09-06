4c0207c007290091995ec3bbd047a40e051e3fdb
liuxiang94 <x-liu12@foxmail.com>
2026-09-06 09:52:12 +0800

    build v1.12.6

--- build.sh
+++ build.sh
@@ -23,11 +23,11 @@ else
     exit -1
 fi
 
-libfilezilla_version=0.50.0
+libfilezilla_version=0.57.0
 libfilezilla_path=$PWD/libfilezilla-windows-$arch
-filezilla_server_version=1.10.1
+filezilla_server_version=1.12.6
 filezilla_server_path=$PWD/filezilla-server-windows-$arch
-wxwidgets_version=3.2.7
+wxwidgets_version=3.2.11
 wxwidgets_path=$PWD/wxmsw-windows-$arch
 
 export PATH=$llvm_dir/bin:$wxwidgets_path/bin:$PATH
